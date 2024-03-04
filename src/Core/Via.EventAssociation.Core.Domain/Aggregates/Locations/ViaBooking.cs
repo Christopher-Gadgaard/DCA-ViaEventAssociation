b48ea58cce85244fd903ecf6eb4f3df66d3eb4be
Vlad Lazar <lazar.vlad151@gmail.com>
2024-03-04 17:22:32 +0100

    booking

---
+++ src/Core/Via.EventAssociation.Core.Domain/Aggregates/Locations/ViaBooking.cs
@@ -0,0 +1,18 @@
+﻿using Via.EventAssociation.Core.Domain.Common.Bases;
+using Via.EventAssociation.Core.Domain.Common.Values;
+using Via.EventAssociation.Core.Domain.Common.Values.Ids;
+
+namespace Via.EventAssociation.Core.Domain.Aggregates.Locations;
+
+public class ViaBooking: Entity<ViaEventId>
+{
+    public ViaBookingId ViaBookingId;
+    
+    private ViaLocationId _viaLocationId;
+    
+    private ViaEventId _viaEventId;
+    
+    private ViaStartDate _viaStartDate;
+
+    
+}
