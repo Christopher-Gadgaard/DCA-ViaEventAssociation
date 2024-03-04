cfc4089d3eb40819c163bf60342363cf4500988f
Vlad Lazar <lazar.vlad151@gmail.com>
2024-03-04 17:28:16 +0100

    guest aggregate and attributes

---
+++ src/Core/Via.EventAssociation.Core.Domain/Aggregates/Guests/ViaGuest.cs
@@ -0,0 +1,17 @@
+﻿using Via.EventAssociation.Core.Domain.Common.Bases;
+using Via.EventAssociation.Core.Domain.Common.Values.Ids;
+
+namespace Via.EventAssociation.Core.Domain.Aggregates.Guests;
+
+public class ViaGuest:AggregateRoot<ViaId>
+{
+    public ViaGuestId ViaGuestId;
+    private ViaGuestName _viaGuestName;
+    private ViaEmail _viaEmail;
+
+
+    public ViaGuest()
+    {
+        
+    }
+}
---
+++ src/Core/Via.EventAssociation.Core.Domain/Aggregates/Guests/ViaGuestId.cs
@@ -0,0 +1,12 @@
+﻿using Via.EventAssociation.Core.Domain.Common.Bases;
+
+namespace Via.EventAssociation.Core.Domain.Aggregates.Guests;
+
+public class ViaGuestId :ValueObject
+{
+    public Guid Value { get; private init; }
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        throw new NotImplementedException();
+    }
+}
